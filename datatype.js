be7d06951bc9d27d5005db1471394fe925d8cc15
EstilRoyce <101394556+EstilRoyce@users.noreply.github.com>
2022-06-22 06:22:58 +0100

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,21 @@
+let number="10"// string
+true// if my firstName is esther, the value is true
+false// if my firstName isn't esther, the value is false
+let b
+console.log(b)
+
+let c=Null
+
+
+
+console.log(typeof "10")
+
+console.log(typeof true)
+
+console.log(typeof b)
+
+console.log(typeof c)
+
+
+
+
