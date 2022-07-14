e917c7c544af214800448993e46433b04c6f1e26
EstilRoyce <101394556+EstilRoyce@users.noreply.github.com>
2022-07-14 03:14:49 +0100

    Delete variable.js

--- Day1/variable.js
+++
@@ -1,9 +0,0 @@
-create a variable.js file and declare variable and assign String, boolean, undefined and null data types
-let a=""// string
-true// if my firstName is esther, the value is true
-false// if my firstName isn't esther, the value is false
-let b // undefines
-console.log(b)
- let b= null
-
-
